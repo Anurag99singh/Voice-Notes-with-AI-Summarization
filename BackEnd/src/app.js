947fdace4b2ad7168392248a2f2ade5c3b08157c
Amurag <anuragsinghisgood@gmail.com>
2025-09-23 14:20:59 +0530

    remove the trailing / from the origin at cors

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://voice-notes-with-ai-summarization.netlify.app/",
+    origin: "https://voice-notes-with-ai-summarization.netlify.app",
     credentials: true,
   })
 );
